99080716288c4d5793b11c78ce7272cec3f1d274
Matthew Johnson <matthew.dave.johnson@gmail.com>
2022-02-11 01:17:34 -0800

    Solution for execution context practice problem 5

---
+++ Lesson-2/less2-exec-con-q5.js
@@ -0,0 +1,20 @@
+/*
+ Use call to invoke the add method but with foo as execution context. What
+ will this return?
+*/
+
+let foo = {
+  a: 1,
+  b: 2,
+};
+
+let bar = {
+  a: 'abc',
+  b: 'def',
+  add: function () {
+    return this.a + this.b;
+  },
+};
+
+console.log(bar.add()); // returns abcdef
+console.log(bar.add.call(foo)); // returns 3
