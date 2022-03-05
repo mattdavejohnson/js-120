c017400ebee4d1da6fbb0857fdc37e648a25d037
Matthew Johnson <matthew.dave.johnson@gmail.com>
2022-03-05 00:21:10 -0800

    Solution for Easy practice problem Rectangles

---
+++ Practice-Problems/Easy/rectangles.js
@@ -0,0 +1,28 @@
+// Rectangles
+// Create a class Rectangle. The constructor should take 2 arguments which
+// represent width and length respectively.
+
+class Rectangle {
+  constructor(width, length) {
+    this.width = width;
+    this.length = length;
+  }
+
+  getWidth() {
+    return this.width;
+  }
+
+  getLength() {
+    return this.length;
+  }
+
+  getArea() {
+    return this.width * this.length;
+  }
+}
+
+let rect = new Rectangle(4, 5);
+
+console.log(rect.getWidth()); // 4
+console.log(rect.getLength()); // 5
+console.log(rect.getArea()); // 20
