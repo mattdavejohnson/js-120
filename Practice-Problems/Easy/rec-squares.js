3a12a962496bf1055471ce75de4a74f0d198e78c
Matthew Johnson <matthew.dave.johnson@gmail.com>
2022-03-05 00:47:41 -0800

    Solution for Easy practice problem Rectangles and Squares

---
+++ Practice-Problems/Easy/rec-squares.js
@@ -0,0 +1,30 @@
+// Rectangles and Squares
+// Write a class called Square that inherits from Rectangle
+
+class Rectangle {
+  constructor(width, length) {
+    this.width = width;
+    this.length = length;
+  }
+
+  getWidth() {
+    return this.width;
+  }
+
+  getLength() {
+    return this.length;
+  }
+
+  getArea() {
+    return this.width * this.length;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+}
+
+let square = new Square(5);
+console.log(`area of square = ${square.getArea()}`);
